93d067ef7cb3f5d7bb80cf793094980cb1b08f3e
rezapasalar <35224879+rezapasalar@users.noreply.github.com>
2021-11-16 12:24:10 +0330

    Update modal.blade.php
    There was a problem when the display of information was higher than the height of the page and we could not scroll to the side of the page, and with these changes the performance remained the same and the scrolling was added.

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -9,6 +9,7 @@ $maxWidth = [
     'lg' => 'sm:max-w-lg',
     'xl' => 'sm:max-w-xl',
     '2xl' => 'sm:max-w-2xl',
+    '3xl' => 'sm:max-w-3xl',
 ][$maxWidth ?? '2xl'];
 @endphp
 
@@ -32,12 +33,15 @@ $maxWidth = [
     }"
     x-init="$watch('show', value => {
         if (value) {
+            document.body.classList.add('events-pointer');
             document.body.classList.add('overflow-y-hidden');
             {{ $attributes->has('focusable') ? 'setTimeout(() => firstFocusable().focus(), 100)' : '' }}
         } else {
+            $el.scrollTo(0, 0)
             document.body.classList.remove('overflow-y-hidden');
         }
     })"
+    x-on:click.self="show = false"
     x-on:close.stop="show = false"
     x-on:keydown.escape.window="show = false"
     x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
@@ -45,17 +49,8 @@ $maxWidth = [
     x-show="show"
     id="{{ $id }}"
     class="jetstream-modal fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
-    style="display: none;"
+    style="display: none; z-index: 999"
 >
-    <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false" x-transition:enter="ease-out duration-300"
-                    x-transition:enter-start="opacity-0"
-                    x-transition:enter-end="opacity-100"
-                    x-transition:leave="ease-in duration-200"
-                    x-transition:leave-start="opacity-100"
-                    x-transition:leave-end="opacity-0">
-        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-    </div>
-
     <div x-show="show" class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
                     x-transition:enter="ease-out duration-300"
                     x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
